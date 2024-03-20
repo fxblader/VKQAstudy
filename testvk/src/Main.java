b7a838c8da1b111b8a589fcca30abc90460af54b
Blader <164267492+fxblader@users.noreply.github.com>
2024-03-21 02:39:51 +0300

    Add files via upload

---
+++ testvk/src/Main.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+public class Main {
+    @Test
+    public void FirstTest() {
+        assertTrue(true);
+    }
+    public void main(){
+        FirstTest();
+    }
+}
